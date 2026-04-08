8423fbe7a6f98972d5dabf8aaffd207972c8a222
zmhaoo <zmhaoo@gmail.com>
2026-04-08 23:50:02 +0800

    Resolve merge conflicts

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun main() {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    SelfIntro().introduce()
     //test  Quick trunk-based change
 
 
