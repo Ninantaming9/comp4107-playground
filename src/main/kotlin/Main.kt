f3e0934aacfb0d9978d90704b509d9cb28f23298
zmhaoo <zmhaoo@gmail.com>
2026-04-08 23:44:00 +0800

    Add self-introduction feature

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 package hk.edu.hkbu.comp.comp4107.playground
 
-import hk.edu.hkbu.comp.comp4107.playground.kennycheng.SelfIntro
+import hk.edu.hkbu.comp.comp4107.playground.zhangminghao.SelfIntro
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
---
+++ src/main/kotlin/zhangminghao/SelfIntro.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.zhangminghao
+
+class SelfIntro {
+    fun introduce() {
+        println("Hello, my name is zhangminghao.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
